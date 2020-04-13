f21a4c3e254accde88f8f2c33739d2e1108c1e69
derek28 <zhengkai28@hotmail.com>
2020-04-13 16:53:28 -0400

    ehs_player updated

--- ehs_player.cpp
+++ ehs_player.cpp
@@ -26,8 +26,27 @@ int GetNumOfActions(GameState game_state, int street) {
 	}
 }
 
+void SetCall(Action &act, LegalActions legal_act) {
+	act.action = 1;
+	act.amount = legal_act.LegalCall.amount;
+}
+
+void SetRaise(Action &act, int amount, LegalActions legal_act) {
+	act.action = 2;
+	act.amount = amount;
+}
+
+void SetFold(Action &act) {
+	act.action = 0;
+}
+
+void SetCheck(Action &act) {
+	act.action = 1;
+	act.amount = 0;
+}
+
 Action EhsPlayer::Act(GameState game_state, LegalActions legal_actions) {
-	Action action_to_return;
+	Action my_action;
 	
 	std::cout << "[AI] My cards are:" << GetHoleCards() << std::endl;
 	std::cout << "[AI] I am at seat " << GetID() << std::endl;
@@ -43,14 +62,14 @@ Action EhsPlayer::Act(GameState game_state, LegalActions legal_actions) {
 	// preflop strategy
 	if (game_state.current_street == 0) {
 		IHS = GetImmediateStrength(my_cards, board, NULL);
+
 		if (GetNumOfActions(game_state, 0) == 0) {
 			// first to act
 			cout << "[AI] I am first to act..." << endl;
 			if (IHS > 0.6) {
-				action_to_return.action = 2;		// raise 
-				action_to_return.amount = 2 * game_state.total_pot_size;
+				SetRaise(my_action, 2 * game_state.total_pot_size, legal_actions);
 			} else {
-				action_to_return.action = 1;		// call
+				SetCall(my_action, legal_actions);
 			}
 		}
 
@@ -58,17 +77,16 @@ Action EhsPlayer::Act(GameState game_state, LegalActions legal_actions) {
 			if (game_state.action_history.preflop[0].player_action.action == 1)  { // opp calls
 				cout << "[AI] OK... you call..." << endl;
 				if (IHS > 0.7) {
-					action_to_return.action = 2;		// raise 
-					action_to_return.amount = 2 * game_state.total_pot_size;
+					SetRaise(my_action, 2 * game_state.total_pot_size, legal_actions);
+				} else {
+					SetCheck(my_action);
 				}
-				action_to_return.action = 1;		// call
 			} else {					// opp raises
 				cout << "[AI] You raised..." << endl;
 				if (IHS > 0.8) {
-					action_to_return.action = 2;		// raise 
-					action_to_return.amount = 2 * game_state.total_pot_size;
+					SetRaise(my_action, 2 * game_state.total_pot_size, legal_actions);
 				} else {
-					action_to_return.action = 1;		// call
+					SetCall(my_action, legal_actions);
 				}
 			}
 		}
@@ -76,28 +94,28 @@ Action EhsPlayer::Act(GameState game_state, LegalActions legal_actions) {
 		if (GetNumOfActions(game_state, 0) >= 2) {
 			cout << "[AI] You raised me!" << endl;
 			if (IHS > 0.8) {
-				action_to_return.action = 1;	// call
+				SetCall(my_action, legal_actions);
 			} else {
-				action_to_return.action = 0;	// fold
+				SetFold(my_action);
 			}
 		}
-		return action_to_return;
 	}	
 
 	// flop
 	if (game_state.current_street == 1) {
-		action_to_return.action = 1;	
+		IHS = GetImmediateStrength(my_cards, board, NULL);
+		SetCall(my_action, legal_actions);	
 	}
 
 	// turn
 	if (game_state.current_street == 2) {
-		action_to_return.action = 1;	
+		SetCall(my_action, legal_actions);	
 	}
 
 	// river
 	if (game_state.current_street == 3) {
-		action_to_return.action = 1;	
+		SetCall(my_action, legal_actions);	
 	}
 
-	return action_to_return;
+	return my_action;
 }
